62e6df4601056281a3c62aa309689ffc831e843b
juno-junho <ssmm0205@flow.team>
2025-02-13 00:52:17 +0900

    feat(authorization): AuthorizationManager 및 AuthorizationDecision 생성

---
+++ src/main/java/nextstep/security/authorization/AuthorizationManager.java
@@ -0,0 +1,10 @@
+package nextstep.security.authorization;
+
+import nextstep.security.authentication.Authentication;
+
+@FunctionalInterface
+public interface AuthorizationManager<T> {
+
+    AuthorizationDecision check(Authentication authentication, T object);
+
+}
